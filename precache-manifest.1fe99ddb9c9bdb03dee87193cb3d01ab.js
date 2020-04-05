6bd3cc8467697f489c6a82786b5621e3648c2655
Timothy Njoroge <njorogetimk@gmail.com>
2020-04-05 15:53:01 +0300

    Updates

---
+++ precache-manifest.1fe99ddb9c9bdb03dee87193cb3d01ab.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58918f661d260f9c4861d6751068b4b1",
+    "url": "/lab/index.html"
+  },
+  {
+    "revision": "6a5f006174781f1f6fc4",
+    "url": "/lab/static/css/main.365ac23d.chunk.css"
+  },
+  {
+    "revision": "5644b2200f9508fb395e",
+    "url": "/lab/static/js/2.f8e6984d.chunk.js"
+  },
+  {
+    "revision": "89ea5e0ed13930f7b9c9c2f392e27cfe",
+    "url": "/lab/static/js/2.f8e6984d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a5f006174781f1f6fc4",
+    "url": "/lab/static/js/main.65c085e2.chunk.js"
+  },
+  {
+    "revision": "9324faf39611bc38ff3e",
+    "url": "/lab/static/js/runtime-main.d1e7f782.js"
+  },
+  {
+    "revision": "8b79a40031b0ea05aa3f6a16bf8a2465",
+    "url": "/lab/static/media/105._Kuhuishwa_kwa_Uwezo_wa_Bi_Arusi.8b79a400.mp3"
+  },
+  {
+    "revision": "8ccac85eb78810fe6c22927728624e82",
+    "url": "/lab/static/media/110._Maisha_Ya_Tai.8ccac85e.mp3"
+  },
+  {
+    "revision": "13a41929e818172ac639ace880ad4a9b",
+    "url": "/lab/static/media/20th anniversary.13a41929.zip"
+  },
+  {
+    "revision": "551b1473b7e5f13c859520da2273aaa7",
+    "url": "/lab/static/media/40._Ukombozi_wetu_wadhihirisha_utukufu_wa_Mungu.551b1473.mp3"
+  },
+  {
+    "revision": "3ab617e1fdb3921f9c10148fea7951f8",
+    "url": "/lab/static/media/60._Yesu_Kristo_Msamaria_Mwema.3ab617e1.mp3"
+  },
+  {
+    "revision": "ef68b45865cef3575813fc601487c82d",
+    "url": "/lab/static/media/93._Bi-Arusi_katika_chumba_cha_kuvaa.ef68b458.mp3"
+  },
+  {
+    "revision": "7804e7e2233e9f81a64f5fd7a45e91d6",
+    "url": "/lab/static/media/98._Bwana_Umebadilisha_Ushuhuda_Wangu.7804e7e2.mp3"
+  },
+  {
+    "revision": "0ef1e5bfbf17ac085c3feeb1eda3270d",
+    "url": "/lab/static/media/Abudu Mwana-Kondoo Mkombozi.0ef1e5bf.mp3"
+  },
+  {
+    "revision": "208f17ad438023e5c9a0c96bc8d3dce0",
+    "url": "/lab/static/media/Bi-Arusi Amekaribia Kwenda.208f17ad.mp3"
+  },
+  {
+    "revision": "e0aa1a32186a00a9dc48c460655eaf45",
+    "url": "/lab/static/media/Bi-Arusi wa Omega Aliyerejeshwa.e0aa1a32.mp3"
+  },
+  {
+    "revision": "8aeb10d328b8cdd7f06d0485b67dea69",
+    "url": "/lab/static/media/Bwana Amwendesha Farasi Tena Katika Bi-Arusi.8aeb10d3.mp3"
+  },
+  {
+    "revision": "d309e45ce61f19d99e0c74305e549308",
+    "url": "/lab/static/media/Eagle Life.d309e45c.zip"
+  },
+  {
+    "revision": "2f570357427967b09338dff4daa07104",
+    "url": "/lab/static/media/Eagle Songs Feb 2020.2f570357.pdf"
+  },
+  {
+    "revision": "13d5843f3528c3bcbcf632f03095176b",
+    "url": "/lab/static/media/Folder.13d5843f.jpg"
+  },
+  {
+    "revision": "7c9993fef6d0522101adcf2720fbf965",
+    "url": "/lab/static/media/Folder.7c9993fe.jpg"
+  },
+  {
+    "revision": "b47c9846d5c7f1828ef0ad643c435ad6",
+    "url": "/lab/static/media/Kutembea na Elohimu Nikamilike.b47c9846.mp3"
+  },
+  {
+    "revision": "e09df9698abf7b840c5c0cc47dc9eb86",
+    "url": "/lab/static/media/Nakuabudu Mungu Baba.e09df969.mp3"
+  },
+  {
+    "revision": "e90575058c26b3c98e54875ab1f1d2e7",
+    "url": "/lab/static/media/Siri ya Muhuri wa Saba.e9057505.mp3"
+  },
+  {
+    "revision": "a17fe560fdba17097255be30d543c7a9",
+    "url": "/lab/static/media/backg.a17fe560.jpg"
+  },
+  {
+    "revision": "7c9993fef6d0522101adcf2720fbf965",
+    "url": "/lab/static/media/logo.7c9993fe.jpg"
+  }
+]);
